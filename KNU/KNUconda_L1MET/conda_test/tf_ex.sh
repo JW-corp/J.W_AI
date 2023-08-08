ff027110dbc2dd5217a851bae7a6248ea8147096
Jkim <jiwoong.kim@cern.ch>
2023-08-08 09:05:30 +0900

    Update tf_ex.sh

--- KNU/KNUconda_L1MET/conda_test/tf_ex.sh
+++ KNU/KNUconda_L1MET/conda_test/tf_ex.sh
@@ -2,22 +2,6 @@
 
 
 
-## >>> conda initialize >>>
-## !! Contents within this block are managed by 'conda init' !!
-#__conda_setup="$('/u/user/jiwoong/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-#if [ $? -eq 0 ]; then
-#    eval "$__conda_setup"
-#else
-#    if [ -f "/u/user/jiwoong/anaconda3/etc/profile.d/conda.sh" ]; then
-#        . "/u/user/jiwoong/anaconda3/etc/profile.d/conda.sh"
-#    else
-#        export PATH="/u/user/jiwoong/anaconda3/bin:$PATH"
-#    fi
-#fi
-#unset __conda_setup
-## <<< conda initialize <<<
-
-
 ls /u/user/jiwoong/anaconda3/etc/profile.d/conda.sh
 export PATH=~/anaconda3/bin:$PATH
 source /u/user/jiwoong/anaconda3/etc/profile.d/conda.sh
@@ -27,8 +11,8 @@ conda-env -h
 conda-env create -f environment.yml
 conda activate l1metml
 
-#
-#echo "#2 CHECKING  GPU status"
-#nvidia-smi
+
+echo "#2 CHECKING  GPU status"
+nvidia-smi
 
 
